7d6d4e91afd519e74d651fc8fc4e52ab1e1f7bb9
Ignacio Sagues <isagues@itba.edu.ar>
2021-11-19 16:41:38 -0300

    ejercicio 2

--- TPE/src/main/java/ar/edu/itba/graph/AirRoutesMain.java
+++ TPE/src/main/java/ar/edu/itba/graph/AirRoutesMain.java
@@ -3,6 +3,9 @@ package ar.edu.itba.graph;
 import static ar.edu.itba.graph.GraphUtils.loadEdges;
 import static ar.edu.itba.graph.GraphUtils.loadGraphML;
 import static ar.edu.itba.graph.GraphUtils.loadVertex;
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.collect_list;
+import static org.apache.spark.sql.functions.sort_array;
 
 import java.io.IOException;
 import java.util.List;
@@ -22,6 +25,10 @@ import ar.edu.itba.graph.models.EdgeProperties;
 import ar.edu.itba.graph.models.VertexProperties;
 
 public class AirRoutesMain {
+
+    private static final Column NEGATIVE_LAT = new Column("a.lat").isNotNull().and(new Column("a.lat").lt(Double.valueOf("0")));
+    private static final Column NEGATIVE_LON = new Column("a.lon").isNotNull().and(new Column("a.lon").lt(Double.valueOf("0")));
+
     public static void main(String[] args) throws IOException {
         final SparkConf spark = new SparkConf().setAppName("TPE - isagues");
         final JavaSparkContext sparkContext = new JavaSparkContext(spark);
@@ -43,6 +50,7 @@ public class AirRoutesMain {
 
         // printGraph(airRoutes);
         ej1(airRoutes);
+        ej2(airRoutes);
 
         sparkContext.close();
     }
@@ -67,30 +75,54 @@ public class AirRoutesMain {
             .filterVertices("labelV = 'airport'")
             .filterEdges("labelE = 'route'")
             .find("(a)-[e]->(b); (b)-[e2]->(c)")
-            .filter(new Column("a.lat").isNotNull().and(new Column("a.lat").lt(Double.valueOf("0"))))
-            .filter(new Column("a.lon").isNotNull().and(new Column("a.lon").lt(Double.valueOf("0"))))
+            .filter(NEGATIVE_LAT)
+            .filter(NEGATIVE_LON)
             .filter("c.code = 'SEA'")
             .filter("a.id != b.id and a.id != c.id and b.id != c.id")
             ;
             
-        final Dataset<Row> oneStopVertex = oneStop.select("a.code", "a.lat", "a.lon", "b.code", "c.code");
-        oneStopVertex.show(1000);
-        oneStopVertex.printSchema();
-        
-        
+            final Dataset<Row> oneStopVertex = oneStop.select("a.code", "a.lat", "a.lon", "b.code", "c.code");
+            oneStopVertex.show(1000);
+            oneStopVertex.printSchema();
+            
+            
         final Dataset<Row> direct = airRoutes
-        .filterVertices("labelV = 'airport'")
-        .filterEdges("labelE = 'route'")
-        .find("(a)-[e]->(b)")
-        .filter(new Column("a.lat").isNotNull().and(new Column("a.lat").lt(Double.valueOf("0"))))
-        .filter(new Column("a.lon").isNotNull().and(new Column("a.lon").lt(Double.valueOf("0"))))
-        .filter("b.code = 'SEA'")
-        .filter("a.id != b.id")
-        ;
+            .filterVertices("labelV = 'airport'")
+            .filterEdges("labelE = 'route'")
+            .find("(a)-[e]->(b)")
+            .filter(NEGATIVE_LAT)
+            .filter(NEGATIVE_LON)
+            .filter("b.code = 'SEA'")
+            .filter("a.id != b.id")
+            ;
         
         final Dataset<Row> directVertex = direct.select("a.code", "a.lat", "a.lon", "b.code");
         
         directVertex.show(1000);
         directVertex.printSchema();
+    }
+    
+    private static void ej2(final GraphFrame airRoutes) {
+        
+        final Dataset<Row> ans = airRoutes
+            .filterEdges("labelE = 'contains'")
+            .find("(c)-[]->(a); (p)-[]->(a)")
+            .filter(col("a.labelV").eqNullSafe("airport"))
+            .filter(col("c.labelV").eqNullSafe("continent"))
+            .filter(col("p.labelV").eqNullSafe("country"))
+            .select(
+                col("c.desc").alias("continent")
+                , col("a.country").alias("country")
+                , col("p.desc").alias("countryDesc")
+                , col("a.elev").alias("elev")
+                )
+            .groupBy(col("continent"), col("country"), col("countryDesc"))
+            .agg(sort_array(collect_list(col("elev"))).alias("elevations"))
+            .sort(col("continent"), col("country"), col("countryDesc"))
+            ;
+            
+        
+        ans.show(1000, false);
+        ans.printSchema();
     }
 }
