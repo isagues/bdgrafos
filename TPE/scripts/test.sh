e9fb9b841f8010d886fdf1e5db56d3ec11524ac9
Ignacio Sagues <isagues@itba.edu.ar>
2021-12-14 21:17:27 -0300

    Scripts

---
+++ TPE/scripts/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+JARS=hdfs:///user/isagues/tpe/blueprints-core-2.6.0.jar
+JARS=$JARS,hdfs:///user/isagues/graphframes-0.8.0-spark2.4-s_2.11.jar  
+
+CLASS_NAME=ar.edu.itba.graph.AditionalQueries
+MAIN_JAR=hdfs:///user/isagues/tpe/original-tpe-isagues-1.jar 
+ARGS=hdfs:///user/isagues/tpe/air-routes.graphml
+
+spark-submit --master yarn --deploy-mode cluster --jars=$JARS --class $CLASS_NAME $MAIN_JAR $ARGS
